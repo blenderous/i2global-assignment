75e0ef06f5b03c39335a7241b8422fb7375f056a
Harish Veerasekaran <hpone.n.only@gmail.com>
2025-01-02 18:56:03 +0530

    created proper post request

--- components/AddNote.tsx
+++ components/AddNote.tsx
@@ -8,14 +8,13 @@ import { PlusIcon } from "@heroicons/react/16/solid";
 import { Button } from "./ui/button";
 import {
   DialogDescription,
-  DialogFooter,
+  // DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "./ui/dialog";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
-
-const wait = () => new Promise((resolve) => setTimeout(resolve, 1000));
+import { revalidatePath } from "next/cache";
 
 export default function AddNote() {
   const [open, setOpen] = useState(false);
@@ -34,32 +33,39 @@ export default function AddNote() {
           <Dialog.Content>
             <DialogHeader>
               <DialogTitle>Create a new note</DialogTitle>
+              <DialogDescription>
+                Enter the title and content of your note. Click submit when you
+                are done.
+              </DialogDescription>
             </DialogHeader>
 
             <form
               onSubmit={(event) => {
-                wait().then(() => setOpen(false));
-
-                // var payload = {
-                //   id: "42",
-                //   text: "Carry a water bottle",
-                //   content: "Avoid buying water in plastic bottles",
-                // };
+                var payload = {
+                  id: "3",
+                  title: "Carry a water bottle",
+                  content: "Avoid buying water in plastic bottles.",
+                };
 
                 // var data = new FormData();
                 // data.append("json", JSON.stringify(payload));
 
-                // fetch("http://localhost:4000/notes", {
-                //   method: "POST",
-                //   body: data,
-                // })
-                //   .then(function (res) {
-                //     return res.json();
-                //   })
-                //   .then(function (data) {
-                //     alert(JSON.stringify(data));
-                //     setOpen(false);
-                //   });
+                fetch("http://localhost:4000/notes", {
+                  method: "POST",
+                  headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(payload),
+                })
+                  .then(function (res) {
+                    return res.json();
+                  })
+                  .then(function (data) {
+                    console.log(data);
+                    revalidatePath("/notes");
+                    setOpen(false);
+                  });
                 event.preventDefault();
               }}
             >
